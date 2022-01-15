000023a04b06f7e31e9db3384a3f884b87eae6a5
aylinbas <ayln.bas.50@hotmail.com>
2022-01-15 22:05:15 +0300

    category

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,29 +9,37 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-
             //ProductManager productManager = new ProductManager(new InMemoryProductDal()); //burada daha sonra inMemory değil entityFramework verse
             //onun kodları çalışacak.
             //Business katmanında hiçbir değişiklik yapmana gerek kalmayacak.
+           // ProductTest();
+            Console.WriteLine("**************************");
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach(var category in categoryManager.GetAll()) {
+                Console.WriteLine(category.CategoryName);
+            }
+
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-        
+
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
             }
-            Console.WriteLine("**************************");
+
             foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
-            Console.WriteLine("**************************");
-            foreach (var product in productManager.GetByUnitPrice(2,100))
+
+            foreach (var product in productManager.GetByUnitPrice(2, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
